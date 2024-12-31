d47bef1e0c847c73389d3103327efe761f8a5805
ZippyTheDoggy <ZippyTheDoggyYT@gmail.com>
2024-12-31 01:35:14 -0600

    project upload

---
+++ include/classes.h
@@ -0,0 +1,159 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <map>
+#include <utility>
+#include <algorithm>
+
+class Item;
+
+/*
+IDEA:
+    Recipe:
+        [0, 1]
+        [2, 3]
+        Offsets from 0,0:
+            0: 0, 0
+            1: 1, 0
+            2: 0, 1
+            3: 1, 1
+        If we scale this to a 3x3, it can either start at the top left corner, or in the middle, and still work.
+        How we can check this?  If each item's offsets, are the same offset from the top left corner
+        In the case of either:
+        [0, 1, 0]      [0, 0, 0]
+        [2, 3, 0]  or  [0, 0, 1]
+        [0, 0, 0]      [0, 2, 3]
+        They're both the exact same recipe, just in a different offset.  In this case,
+        the offset is 1, 1, as the original recipe, in a 4x4 manner, is offset by 1 to the right, 1 down, to start in the center of the grid.
+*/
+
+class Recipe {
+    private:
+        std::vector<std::pair<Item*, int>> _recipe;
+        std::map<Item*, int> _counts;
+    public:
+        Recipe(std::vector<std::pair<Item*, int>> recipe) {
+            for(std::pair<Item*, int>& pair : recipe) {
+                const auto& [item, count] = pair;
+                if(std::find(_recipe.begin(), _recipe.end(), pair) == _recipe.end())
+                    _recipe.push_back(std::make_pair(item, count));
+                if(_counts.find(item) == _counts.end())
+                    _counts[item] = count;
+                else _counts[item] += count;
+            }
+        }
+        std::vector<Item*> getItems() {
+            std::vector<Item*> items;
+            for(std::pair<Item*, int>& pair : _recipe) {
+                if(std::find(items.begin(), items.end(), pair.first) == items.end())
+                    items.push_back(pair.first);
+            }
+            return items;
+        }
+        int getCount(Item* item) {
+            if(_counts.find(item) == _counts.end()) return -1;
+            return _counts.at(item);
+        }
+};
+
+class Item {
+    private:
+        int _id = -1;
+        std::string _name;
+        inline static int _next_id = 1;
+    public:
+        std::vector<Recipe*> recipes;
+        Item(std::string name) {
+            _name = name;
+            _id = Item::NextId();
+        }
+
+        std::string getName() {
+            return _name;
+        }
+        int getId() {
+            return _id;
+        }
+
+        int getNeededItems(Item* item) {
+            int count = 0;
+            for(Recipe* recipe : recipes) {
+                std::vector<Item*> items = recipe->getItems();
+                if(std::find(items.begin(), items.end(), item) != items.end()) {
+                    count += recipe->getCount(item);
+                }
+            }
+            if(count == 0) return -1;
+            return count;
+        }
+        std::vector<std::pair<Item*, int>> getItemCounts() {
+            std::vector<std::pair<Item*, int>> counts;
+            for(Recipe* recipe : recipes) {
+                std::vector<Item*> items = recipe->getItems();
+                for(Item* item : items) {
+                    if(item->recipes.size() > 0) {
+                        for(auto& [i, count] : item->getItemCounts()) {
+                            std::vector<std::pair<Item*, int>>::iterator fi = std::find_if(counts.begin(), counts.end(), [i](std::pair<Item*, int> e) {
+                                return e.first == i;
+                            });
+                            if(fi != counts.end()) {
+                                fi->second += count * recipe->getCount(item);
+                            } else {
+                                counts.push_back(std::make_pair(i, count * recipe->getCount(item)));
+                            }
+                        }
+                    } else
+                        counts.push_back(std::make_pair(item, recipe->getCount(item)));
+                }
+            }
+            return counts;
+        }
+
+        void addRecipe(Recipe* recipe) {
+            recipes.push_back(recipe);
+        }
+
+        static int NextId() {
+            return _next_id++;
+        }
+};
+
+class Counter {
+    private:
+        Item* _item;
+        int _count = 1;
+    public:
+        Counter(Item* item, int count = 1) {
+            _item = item;
+            _count = count;
+        }
+        Item* getItem() {
+            return _item;
+        }
+        int getCount() {
+            return _count;
+        }
+
+        std::vector<std::pair<Item*, int>> getItemCounts() {
+            std::vector<std::pair<Item*, int>> counts;
+            auto a = _item->getItemCounts();
+            for(auto& [item, count] : a) {
+                if(std::find_if(counts.begin(), counts.end(), [item](std::pair<Item*, int> c) {
+                    return c.first == item;
+                }) == counts.end())
+                    counts.push_back(std::make_pair(item, count * _count));
+            }
+            return counts;
+        }
+
+        static void print(Counter counter) {
+            std::cout << counter.getItem()->getName() << " (" << counter.getCount() << "): {\n";
+            for(auto& [item, count] : counter.getItemCounts()) {
+                std::cout << "\t" << item->getName() << ": " << count << "\n";
+            }
+            std::cout << "}" << std::endl;
+        }
+        static void print(Item* item) {
+            print(Counter(item, 1));
+        }
+};
---
+++ main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+#include "include/classes.h"
+
+int main() {
+
+	Item* steel_ingot = new Item("Steel Ingot");
+	Item* glass = new Item("Glass");
+	
+	Item* iron_ingot = new Item("Iron Ingot");
+	Item* iron_gear = new Item("Iron Gear");
+	Item* machine_core = new Item("Machine Core");
+	
+	Item* machine_frame = new Item("Machine Frame");
+
+	Recipe* iron_ingot__to__iron_gear = new Recipe({
+		{iron_ingot, 4}
+	});
+	Recipe* recipe_machine_core = new Recipe({
+		{iron_ingot, 3},
+		{iron_gear, 3}
+	});
+	Recipe* recipe_machine_frame = new Recipe({
+		{steel_ingot, 2},
+		{glass, 3},
+		{machine_core, 4},
+	});
+
+    iron_gear->addRecipe(iron_ingot__to__iron_gear);
+    machine_core->addRecipe(recipe_machine_core);
+    machine_frame->addRecipe(recipe_machine_frame);
+
+	// EXPECTED RECIPE:
+	/*
+		Steel Ingot: 2
+		Glass: 3
+		Iron Ingot: 60
+	*/
+
+    // ONE ITEM:
+    Counter::print(machine_frame);
+
+    // MULTIPLE ITEMS:
+    Counter counter(machine_frame, 5);
+    Counter::print(counter);
+
+    return EXIT_SUCCESS;
+}
